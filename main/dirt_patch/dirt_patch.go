404600a48cd92bfd6a1d98c9309274ffc601f5ce
Tom Cheng <thomascheng1998@googlemail.com>
2019-04-27 23:57:32 +0100

    Added growth bar

--- main/dirt_patch/dirt_patch.go
+++ main/dirt_patch/dirt_patch.go
@@ -51,3 +51,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "bar_sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/bar.tilesource\"\n"
+  "default_animation: \"0\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: -7.0
+    y: 6.0
+    z: 1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
