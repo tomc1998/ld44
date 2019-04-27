dc835337b55610902ab3333ace75ea871a7150f3
Tom Cheng <thomascheng1998@googlemail.com>
2019-04-27 19:29:00 +0100

    Added splashing with the water button

--- main/player/player.go
+++ main/player/player.go
@@ -33,3 +33,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "splash_factory"
+  type: "factory"
+  data: "prototype: \"/main/splash/splash.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
