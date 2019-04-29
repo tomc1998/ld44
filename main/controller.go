8370503096d5fa309c59ea982cef731110b24573
Tom Cheng <thomascheng1998@googlemail.com>
2019-04-29 22:17:17 +0100

    Strung together gui

---
+++ main/controller.go
@@ -0,0 +1,105 @@
+components {
+  id: "controller"
+  component: "/main/controller.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "game"
+  type: "collectionproxy"
+  data: "collection: \"/main/game.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "eol_screen"
+  type: "collectionproxy"
+  data: "collection: \"/main/eol_screen/eol_screen.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "game_over_screen"
+  type: "collectionproxy"
+  data: "collection: \"/main/game_over_screen/game_over_screen.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "main_screen"
+  type: "collectionproxy"
+  data: "collection: \"/main/main_screen/main_screen.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "shop_screen"
+  type: "collectionproxy"
+  data: "collection: \"/main/shop_screen/shop_screen.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- main/state.lua
+++ main/state.lua
@@ -1,5 +1,17 @@
 local mod = {
-   money = 0
+   money = 1000,
+   earnings = 0,
+   carry_over = 0,
 }
+
+function mod:earn_money(money)
+   self.money = self.money + money
+   self.earnings = self.earnings + money
+end
+
+function mod:carry_over_money()
+   self.earnings = 0
+   self.carry_over = self.money
+end
 
 return mod
